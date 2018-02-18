19dbb95e2b77b581fe030aea20edcf3369fd401c
Per Zimmerman <per.zimmerman@gmail.com>
2018-02-18 21:07:00 +0100

    First version

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
+// Find declared globals from es5 source files
+
+const _ = require('lodash')
+const CLIEngine = require('eslint').CLIEngine
 const escope = require('escope')
 const esprima = require('esprima')
-const CLIEngine = require('eslint').CLIEngine
-const _ = require('lodash')
 const fs = require('fs')
 const path = require('path')
-const jscodeshift = require('jscodeshift')
+const program = require('commander')
 
-// TODO: get from file/args
-// SETTINGS
-const externals = {
-  $: 'jquery',
-  jQuery: 'jquery',
-  ko: 'knockout'
+const cli = new CLIEngine()
+
+program
+  .version('0.1')
+  .option('-g, --graph', 'Create expanded dotfile')
+  .option('-m, --minigraph', 'Create mini dotfile')
+  .option('-i, --info <namepattern>', 'Log something')
+  .option('-c, --codemod', 'Create codemod file')
+  .usage('[options] <file ...>')
+  .parse(process.argv)
+
+const [filesRoot, includeFiles] = program.args || ['', '']
+
+// ensure files patterns given
+if (filesRoot.length === 0) {
+  console.error('No source root folder')
+  process.exit(1)
 }
 
-// TODO: get from cli as args
-// TODO: do as prettier cli and write to std out
-// TODO: and write with parameter --write
-const filesRoot = 'source'
-const includeFiles = `${filesRoot}/**/*.js`
-const destinationRoot = 'dest'
+if (includeFiles.length === 0) {
+  console.error('No source root folder')
+  process.exit(1)
+}
 
-//
-// Program
-//
-const cli = new CLIEngine()
 const report = cli.executeOnFiles([includeFiles])
-const modulesRoot = path.resolve(process.cwd(), filesRoot)
+const modulesRoot = path.resolve(filesRoot)
 
 const reportWithGlobals = report.results.map(r => {
-  let { source } = r
+  let { source, filePath } = r
   if (!source) {
-    source = r.source = fs.readFileSync(r.filePath, 'utf-8')
+    source = r.source = fs.readFileSync(filePath, 'utf-8')
   }
   const globals = findGlobals(source)
+  const undefVariables = getUndefinedVariables(r)
+  const module = getModule(filePath)
+
   return {
     ...r,
-    globals
+    globals,
+    undefVariables,
+    module
   }
 })
+
+const globalMap = _.keyBy(
+  _.flatten(
+    reportWithGlobals.map(r => {
+      const { filePath, globals, module } = r
+      return globals.map(({name, type}) => ({
+        filePath,
+        name,
+        module,
+        type
+      }))
+    })
+  ),
+  'name'
+)
 
 const filePaths = _.uniq(report.results.map(r => r.filePath))
 const modules = filePaths.map(filePath => ({
@@ -46,26 +73,163 @@ const modules = filePaths.map(filePath => ({
   ...getModule(filePath)
 }))
 
-const modulesByFilePath = _.keyBy(modules, 'filePath')
+if (program.graph || program.minigraph) {
+  createDotFile(program.minigraph)
+  process.exit(0)
+}
 
-const globalMap = _.keyBy(
-  _.flatten(
-    reportWithGlobals.map(r => {
-      const { filePath, globals } = r
-      const module = getModule(filePath)
+if (program.info) {
+  console.log(globalMap)
 
-      return globals.map(name => ({
-        filePath,
-        name,
-        module
-      }))
+  logjson(reportWithGlobals.filter(r => r.filePath.includes(program.info)).map(({filePath, globals, undefVariables}) => ({
+    filePath,
+    globals,
+    undefVariables
+  })))
+
+  process.exit(0)
+}
+
+if (program.codemod) {
+  // output codemod file
+  const mainTemplate = (data) => `
+var data = ${JSON.stringify(data, null, 2)}
+
+module.exports = function(fileInfo, api) {
+  var path = fileInfo.path
+  var source = fileInfo.source
+  var transform = data[fileInfo.path]
+
+  if (transform) {
+    var root = api.jscodeshift(source)
+    var globals = transform.globals
+    var imports = transform.imports
+    var exports = transform.exports
+    var body = root.get().node.program.body
+
+    if (imports.length > 0) {
+      body.unshift(imports)
+    }
+
+    if (globals.length > 0) {
+      body.unshift(globals)
+    }
+
+    if (exports.length > 0) {
+      body.push(exports)
+    }
+
+    return root.toSource()
+  } else {
+    return source
+  }
+}
+`
+
+  const sourceData = getCodeModData()
+  const data = {}
+  Object.keys(sourceData).forEach(filePath => {
+    const item = sourceData[filePath]
+
+    data[filePath] = {
+      globals: item.globals.length ? `/* global ${item.globals.join(' ')} */` : '',
+      imports: item.imports.map(i => `import {${i.variables.join(', ')}} from '${i.module}'`).join('\n'),
+      exports: item.exports.length ? `export {${item.exports.join(', ')}}` : ''
+    }
+  })
+
+  console.log(mainTemplate(data))
+  process.exit(0)
+}
+
+function createDotFile (mini) {
+  const createDotFileString = (clusters, connections) => `
+digraph {
+  rankdir = LR;
+  graph [fontname = "helvetica"];
+  node [fontname = "helvetica"];
+  edge [fontname = "helvetica"];
+  node [shape = box, style = "filled", color = "#cccccc"];
+
+  ${clusters}
+
+  ${connections}
+}
+`
+
+  const createDotFileClusterString = (index, label, nodeNames) => `
+  subgraph cluster_${index} {
+    fillcolor = "#f0f0f0";
+    style = "filled";
+    pencolor = "transparent";
+
+    label = "${label}";
+
+    "${nodeNames.join('";\n    "')}";
+  }
+  `
+
+  const createDotFileConnectionsString = (connections) => {
+    return connections.map(({ from, to, label, type }) => {
+      const labelString = label ? ` [label = "${label}"]` : ''
+      const colorString = type !== 'FunctionName' ? '"#ff0000"' : '"#00ff00"'
+      return `  "${from}" -> "${to}"${labelString}[color = ${colorString}];`
+    }).join('\n')
+  }
+
+  const uniqueFolders = _.uniq(modules.map(m => m.relativeFromRoot))
+  const clustersString = uniqueFolders.map((folder, index) => {
+    const filesForFolder = reportWithGlobals.filter(r => r.module.relativeFromRoot === folder)
+
+    const clusterInfo = filesForFolder.map(r => {
+      const { module } = r
+      const {name} = module
+      return name
     })
-  ),
-  'name'
-)
 
-const result = reportWithGlobals.map(r => {
-  const { messages, source, globals, filePath } = r
+    return createDotFileClusterString(index, folder, clusterInfo)
+  }).join('\n')
+
+  const connections = getConnections()
+
+  if (mini) {
+    const squishedConnectionsMap = {}
+    connections.forEach(c => {
+      const {from, to, label} = c
+      const key = `${from}->${to}`
+
+      // take first found item and count items
+      if (!squishedConnectionsMap[key]) {
+        if (label) {
+          const count = connections.filter(c2 => c2.from === from && c2.to === to).length
+          squishedConnectionsMap[key] = {
+            from,
+            to,
+            label: count > 1 ? count : undefined
+          }
+        } else {
+          squishedConnectionsMap[key] = {
+            from,
+            to
+          }
+        }
+      }
+    })
+    const connectionKeys = Object.keys(squishedConnectionsMap)
+    const squishedConnections = connectionKeys.map(k => squishedConnectionsMap[k])
+
+    console.log(createDotFileString(clustersString, createDotFileConnectionsString(squishedConnections)))
+  } else {
+    console.log(createDotFileString(clustersString, createDotFileConnectionsString(connections)))
+  }
+}
+
+function logjson (value) {
+  console.log(JSON.stringify(value, null, 2))
+}
+
+function getUndefinedVariables (report) {
+  const { messages } = report
   const uniqueMessages = _.uniqBy(messages, m => m.message)
   const noUndefMessages = uniqueMessages.filter(m => m.ruleId === 'no-undef')
   const undefVariables = noUndefMessages
@@ -74,113 +238,20 @@ const result = reportWithGlobals.map(r => {
       const [, variableName] = message.match(/^'([^']+)'/)
       return variableName
     })
-    .filter(v => {
-      const moduleWithGlobal = globalMap[v]
 
-      if (moduleWithGlobal) {
-        return moduleWithGlobal.filePath !== filePath
-      } else {
-        return true
-      }
-    })
-    .sort()
-
-  const fromModule = modulesByFilePath[filePath]
-  const imports = undefVariables.map(v => {
-    const moduleWithGlobal = globalMap[v]
-
-    if (moduleWithGlobal) {
-      const toModule = modulesByFilePath[moduleWithGlobal.filePath]
-      const i = getImport(fromModule, toModule)
-      return {
-        name: moduleWithGlobal.name,
-        module: i
-      }
-    } else if (externals[v]) {
-      return {
-        name: v,
-        module: externals[v]
-      }
-    } else {
-      return {
-        name: v,
-        module: v
-      }
-    }
-  })
-
-  const root = jscodeshift(source)
-  const importsByModule = _.groupBy(imports, 'module')
-  const importsString = _.keys(importsByModule)
-    .map(module => {
-      const names = importsByModule[module].map(m => m.name)
-      return `import { ${names.join(', ')} } from '${module}'`
-    })
-    .join('\n')
-  const exportsString = globals.map(name => `export { ${name} }`).join('\n')
-
-  if (importsString.length > 0) {
-    root.get().node.program.body.unshift(`${importsString}\n`)
-  }
-
-  if (exportsString.length > 0) {
-    root.get().node.program.body.push(`\n${exportsString}`)
-  }
-
-  const newSource = root.toSource()
-
-  const newFilePath = path.resolve(
-    path.join(destinationRoot, path.relative(filesRoot, filePath))
-  )
-
-  return {
-    source: newSource,
-    filePath,
-    newFilePath
-  }
-})
-
-result.forEach(m => {
-  fs.writeFileSync(m.newFilePath, m.source, 'utf-8')
-})
-
-logjson(result)
-
-//
-// Functions
-//
-function getImport (fromModule, toModule) {
-  return (
-    (fromModule.toRoot === '' ? './' : '') +
-    path.join(fromModule.toRoot, toModule.module)
-  )
-}
-
-function logjson (value) {
-  console.log(JSON.stringify(value, null, 2))
-}
-
-function getModuleForGlobal (name, currentFilePath) {
-  // TODO: compute relative to source root and make import
-  // ${currentModuleRelativeToRoot}/${moduleName}
-  // currentModuleRelativeToRoot = ../.. for currentModule
-  // moduleName = base/the_module
-
-  const { filePath, moduleName } = globalMap[name]
-  // const relativePath = path.relative(path.dirname(filePath), path.dirname(currentFilePath))
-  // const importName = path.join(relativePath, path.basename(filePath, ))
-
-  return {
-    name
-  }
+  return undefVariables
 }
 
 function getModule (filePath) {
   const dirName = path.dirname(filePath)
-  const relativeToRoot = path.relative(modulesRoot, dirName)
+  const relativeFromRoot = path.relative(modulesRoot, dirName)
   const relativeToModule = path.relative(dirName, modulesRoot)
+  const name = path.basename(filePath, '.js')
+
   const module = {
-    module: path.join(relativeToRoot, path.basename(filePath, '.js')),
+    name,
+    relativeFromRoot,
+    module: path.join(relativeFromRoot, name),
     toRoot: relativeToModule
   }
 
@@ -190,14 +261,100 @@ function getModule (filePath) {
 // ref: https://ariya.io/2013/09/scope-analysis-for-javascript-code
 function findGlobals (source) {
   const syntax = esprima.parse(source, { loc: true })
-  const globalScope = escope.analyze(syntax)
-  const implicitVariables = _.flatten(
-    globalScope.globalScope.implicit.variables.map(v =>
-      v.identifiers.map(id => id.name)
-    )
-  )
-  const variables = globalScope.globalScope.variables.map(v => v.name)
+  const scopeManager = escope.analyze(syntax)
+
+  const getValue = v => ({
+    name: v.name,
+    type: v.defs[0].type
+  })
+
+  const implicitVariables = scopeManager.globalScope.implicit.variables.map(getValue)
+  const variables = scopeManager.globalScope.variables.map(getValue)
   const result = implicitVariables.concat(variables).sort()
 
   return result
+}
+
+function getConnections () {
+  const connections = _.flatten(reportWithGlobals.map(r => {
+    const { module, undefVariables } = r
+    const from = module.name
+    return undefVariables.map(variable => {
+      const toModule = globalMap[variable]
+
+      if (toModule) {
+        return {
+          from,
+          to: toModule.module.name,
+          label: variable,
+          type: toModule.type
+        }
+      } else {
+        return {
+          from,
+          to: variable
+        }
+      }
+    }).filter(s => s !== null)
+  }))
+
+  return connections
+}
+
+function getCodeModData () {
+  const dataByFile = reportWithGlobals.map(r => {
+    const {undefVariables, globals, filePath} = r
+    const sourceModule = r.module
+
+    const globalVariables = _.sortBy(undefVariables.map(variable => {
+      const toModule = globalMap[variable]
+
+      if (!toModule) {
+        return variable
+      } else {
+        return null
+      }
+    }).filter(s => s !== null),
+    [o => o.to])
+
+    const importsByModule = _.groupBy(undefVariables.map(variable => {
+      const fromModuleInfo = globalMap[variable]
+
+      if (fromModuleInfo) {
+        const fromModule = fromModuleInfo.module
+        const isInSameFolder = sourceModule.toRoot === fromModule.toRoot && sourceModule.relativeFromRoot === fromModule.relativeFromRoot
+        const relativeModuleImport = isInSameFolder ? `./${fromModule.name}` : `${fromModule.toRoot}/${fromModule.relativeFromRoot}/${fromModule.name}`
+        return {
+          from: relativeModuleImport,
+          name: variable
+        }
+      } else {
+        return null
+      }
+    }).filter(s => s !== null), 'from')
+    const imports = Object.keys(importsByModule).map(modulePath => {
+      return {
+        module: modulePath,
+        variables: importsByModule[modulePath].map(i => i.name)
+      }
+    })
+
+    const exports = globals.map(g => g.name).sort()
+
+    return {
+      filePath,
+      data: {
+        globals: globalVariables,
+        imports,
+        exports
+      }
+    }
+  })
+
+  const data = {}
+  dataByFile.forEach(o => {
+    data[o.filePath] = o.data
+  })
+
+  return data
 }
